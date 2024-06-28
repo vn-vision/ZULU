bf029c5d1e07783d2a3efbbb41b045337c9e2e48
kZilla <kzilla@vn-z.zilla>
2024-06-29 00:42:30 +0300

    Onto the next model/API

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.api_index, name='index'),
     path('users/', views.UserView.as_view(), name='users'),
+    path('users/<int:id>/', views.UserView.as_view(), name='users'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework import status
@@ -11,7 +11,13 @@ class UserView(APIView):
     '''
     Create get and post methods for a user 
     '''
-    def get(self, request, *args, **kwargs):
+    def get(self, request, id=None):
+        # get a single user by id
+        if id:
+            result = Users.objects.get(id=id)
+            serializers = UserSerializer(result);
+            return Response({'status':'success', 'users':serializers.data}, status=200)
+        
         result = Users.objects.all()
         serializers = UserSerializer(result, many=True)    
         return Response({'status': 'success', 'users': serializers.data}, status=200)
@@ -24,6 +30,32 @@ class UserView(APIView):
             return Response({'status':'success', 'data': serializer.data}, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def patch(self, request, id):
+        #update  a user with the id <id>
+        '''
+        Fetch the given id record that is to be update.
+        Pass to the StudentSerializer to convert into JSON.
+        The data received from the request.
+        The partial=True indicates that this may not contains all the fields of our Student model.
+        '''
+
+        try:
+            result = Users.objects.get(id=id)
+            serializer = UserSerializer(result, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({'status':'success', 'data':serializer.data})
+            else:
+                return Response({'status':'error', 'data':serializer.errors})
+        except:
+            return Response({'status':'error', 'message':'Check your request and try again'})
+    
+    def delete(self, request, id=None):
+        # delete a record <id> if it exists
+        result = get_object_or_404(Users, id=id)
+        result.delete()
+        return Response({"status":"success", "data":"Record deleted"})
 
 
 # index view, improve on it
