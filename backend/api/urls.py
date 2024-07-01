c803268e1a08c9c2955a925a1ce06f17f520b487
kZilla <kzilla@vn-z.zilla>
2024-07-01 12:22:24 +0300

    Moving to authentication

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.api_index, name='index'),
-    path('users/', views.UserView.as_view(), name='users'),
-    path('users/<int:id>/', views.UserView.as_view(), name='users'),
+    path('users/', views.UserView.as_view(), name='users-list'), # For GET and POST
+    path('users/<int:id>/', views.UserView.as_view(), name='users-detail'), # For PATCH and DELETE
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import JsonResponse
+from rest_framework import permissions, status
 from rest_framework.views import APIView
-from rest_framework import status
 from rest_framework.response import Response
 from .models import Users
 from .serializers import UserSerializer
@@ -9,27 +9,31 @@ from .serializers import UserSerializer
 # class based view for user
 class UserView(APIView):
     '''
-    Create get and post methods for a user 
+    Class based view for the user model.
+    It handles the GET, POST, PATCH and DELETE request.
     '''
+
     def get(self, request, id=None):
         # get a single user by id
         if id:
-            result = Users.objects.get(id=id)
-            serializers = UserSerializer(result);
-            return Response({'status':'success', 'users':serializers.data}, status=200)
+            result = get_object_or_404(Users, id=id)
+            serializer = UserSerializer(result)
+            return Response({'status':'success', 'users':serializer.data}, status=status.HTTP_200_OK)
         
         result = Users.objects.all()
-        serializers = UserSerializer(result, many=True)    
-        return Response({'status': 'success', 'users': serializers.data}, status=200)
+        serializers = UserSerializer(result, many=True)   
+        return Response({'status': 'success', 'users': serializers.data}, status=status.HTTP_200_OK)
 
 
     def post(self, request):
+        '''
+        Create a new user record.
+        '''
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response({'status':'success', 'data': serializer.data}, status=status.HTTP_200_OK)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     def patch(self, request, id):
         #update  a user with the id <id>
@@ -39,26 +43,24 @@ class UserView(APIView):
         The data received from the request.
         The partial=True indicates that this may not contains all the fields of our Student model.
         '''
+        result = get_object_or_404(Users, id=id)
+        serializer = UserSerializer(result, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'status':'success', 'data':serializer.data}, status=status.HTTP_200_OK)
+        return Response({'status':'error', 'data':serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
-        try:
-            result = Users.objects.get(id=id)
-            serializer = UserSerializer(result, data=request.data, partial=True)
-            if serializer.is_valid():
-                serializer.save()
-                return Response({'status':'success', 'data':serializer.data})
-            else:
-                return Response({'status':'error', 'data':serializer.errors})
-        except:
-            return Response({'status':'error', 'message':'Check your request and try again'})
-    
     def delete(self, request, id=None):
         # delete a record <id> if it exists
         result = get_object_or_404(Users, id=id)
         result.delete()
-        return Response({"status":"success", "data":"Record deleted"})
+        return Response({"status":"success", "data":"Record deleted"}, status=status.HTTP_204_NO_CONTENT)
 
 
 # index view, improve on it
 def api_index(request):
     return JsonResponse({"Greeting" :"Hello, world. You're at the zulu index."})
 
+
+
+    
