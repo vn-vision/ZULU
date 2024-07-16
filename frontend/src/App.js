0ececf66c102678b9eb7ec5296913b92fa7df1b0
kZilla <kzilla@vn-z.zilla>
2024-07-16 12:22:56 +0300

    Landing Page Design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,8 @@
-import logo from './assets/images/logo.png';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Zulu RE<br/>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p> This is the main src app, Call components to display them...</p>
-      </header>
-    </div>
-  );
-}
+  return (<div>
 
+  </div>);
+}
 export default App;
